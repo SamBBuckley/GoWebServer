f474fa8a5475b6706709aa8525722b0492218cd2
Samuel Buckley <SamBBuckley@tamu.edu>
2020-01-23 16:40:14 -0600

    initial commit

---
+++ main/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+type TestData struct {
+	Field1 string
+	Field2 string
+	Filed3 int
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hi")
+	})
+
+	http.HandleFunc("/api/v1/test", func(w http.ResponseWriter, r *http.Request) {
+		data :=TestData{
+			"Field 1",
+			"File 2",
+			4,
+		}
+
+		dataJson, err := json.Marshal(data)
+		if(err != nil) {
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type","application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(dataJson)
+
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
